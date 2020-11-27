5722ebd90604efee0a2a2c5fb1f5ead0a1974c32
hurukou <75116690+hurukou@users.noreply.github.com>
2020-11-27 19:40:58 +0900

    Create sidebar.php

---
+++ sidebar.php
@@ -0,0 +1,5 @@
+<section id="sidebar">
+    <a href="mypage.php" class="side sidemypage">マイページトップ</a>
+    <a href="prof.php" class="side sideprof">プロフィール編集</a>
+    <a href="newProduct.php" class="side sideproduct">生徒を募集</a>
+</section>
