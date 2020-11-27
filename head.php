21e6c866a76a8060921220186594ae6f51d52a8d
hurukou <75116690+hurukou@users.noreply.github.com>
2020-11-27 19:37:26 +0900

    Create head.php

---
+++ head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8">
+        <title>Poker Coach</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+        <!-- フォントアイコン -->
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet">
+    </head>
