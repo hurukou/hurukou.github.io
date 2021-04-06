334b630f7e013872111e229b5bea46fcd3539b1c
Daisuke55 <daisukeippo@gmail.com>
2021-04-06 23:12:54 +0900

    index.phpの更新

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php
-
 //共通変数・関数ファイルを読込み
 require('function.php');
-
-
 //ページネーション
 $paginationNum = (!empty($_GET['p'])) ? $_GET['p'] : 1;
 
@@ -13,14 +10,14 @@ $category = (!empty($_GET['c_id'])) ? $_GET['c_id'] : '';
 $sort = (!empty($_GET['sort'])) ? ($_GET['sort']) : '';
 
 //パラメータに不正な値があるかチェック
-if(!is_int($paginationNum)){
+if (!is_int($paginationNum)) {
     error_log('エラー:不正な値が検出されました');
     header("Location:index.php");
 }
 //表示リスト
 $listCount = 20;
 //現在の表示リストの先頭を検出
-$paginationMinNum = (($paginationNum-1)*$listCount);
+$paginationMinNum = (($paginationNum - 1) * $listCount);
 //DBから商品データを取得
 $dbProduct = getProductlist($paginationMinNum, $category, $sort);
 //DBからカテゴリーデータを取得
@@ -32,56 +29,61 @@ require('head.php');
 ?>
 
 <body class="page-2colum">
-    
+
     <!--　ヘッダー -->
     <?php
-       require('header.php');
+    require('header.php');
     ?>
-    
-        <div class="figure">
-            <img src="img/poker4.jpg" width="2000" height="0" id="top-baner">
-            <div class="text1">Poker Coach</div>
-            <div class="text2">やってみたけど、よくわからない・・・基礎からみっちり教えてもらいたい・・・</div>
-            <div class="text3">ポーカーで稼げるようになりたい・・・という方必見。</div> <div class="text4">上達したいプレイヤーとコーチを繋ぐサイト</div>
-            <a href="" class="btn-beginner">初めての方へ</a>
+
+    <div class="figure">
+        <img src="img/poker4.jpg" width="2000" height="0" id="top-baner">
+        <div class="text1">Poker Coach</div>
+        <div class="text2">やってみたけど、よくわからない・・・基礎からみっちり教えてもらいたい・・・</div>
+        <div class="text3">ポーカーで稼げるようになりたい・・・という方必見。</div>
+        <div class="text4">上達したいプレイヤーとコーチを繋ぐサイト</div>
+        <a href="" class="btn-beginner">初めての方へ</a>
+        <?= $_SESSION['user_id'] ?>
+        <?php if (empty($_SESSION['user_id'])) : ?>
+            <!-- セッションにユーザーIDがある場合、会員登録を投稿ボタンに変更　-->
             <a href="signup.php" class="btn-user">無料会員登録</a>
+        <?php else : ?>
+            <a href="newproduct.php" class="btn-user">投稿する</a>
+        <?php endif; ?>
     </div>
     <!--メインコンテンツ-->
     <section id="main">
         <p>コーチ一覧</p>
-    <div class="page-title">
-        <div class="page-left">
-            <span class="total-num">
-                <?php echo sanitize($dbProduct['total']); ?>
-            </span>件のコーチが見つかりました
+        <div class="page-title">
+            <div class="page-left">
+                <span class="total-num">
+                    <?php echo sanitize($dbProduct['total']); ?>
+                </span>件のコーチが見つかりました
+            </div>
+            <div class="page-right">
+                <span class="num"><?php echo (!empty($dbProduct['data'])) ? $paginationMinNum + 1 : 0; ?></span> - <span class="num"><?php echo $paginationMinNum + count($dbProduct['data']); ?></span>件 /
+                <span class="num"><?php echo sanitize($dbProduct['total']); ?></span>件中
+            </div>
         </div>
-        <div class="page-right">
-            <span class="num"><?php echo (!empty($dbProduct['data'])) ? $paginationMinNum+1 : 0; ?></span> - <span class="num"><?php echo $paginationMinNum+count($dbProduct['data']); ?></span>件 / 
-            <span class="num"><?php echo sanitize($dbProduct['total']); ?></span>件中
-        </div>
-    </div>
-    <div class="page-list">
-        <?php
-        foreach($dbProduct['data'] as $key => $val):
-        ?>
-        <a href="product.php><?php echo (!empty(getPara())) ?
-        getPara().'&p_id='.$val['id'] : '?p_id='.$val['id']; ?>" class="page">    
-        <div class="page-head">
-            <img src="<?php echo sanitize($val['pic']); ?>" alt="<?php  echo sanitize($val['title']); ?>">
-        </div>
-        <div class="page-body">
-            <p class="page-title"><?php echo sanitize($val['title']); ?> <span class="price">¥<?php echo sanitize(number_format($val['price']));?></span>
-            </p>
-        </div>
-        </a>
-        <?php
-          endforeach;
-        ?>
+        <div class="page-list">
+            <?php
+            foreach ($dbProduct['data'] as $key => $val) :
+            ?>
+                <a href="product.php<?php echo (!empty(getPara())) ? getPara() . '&p_id=' . $val['id'] : '?p_id=' . $val['id']; ?>" class="page">
+                    <div class="page-head">
+                        <img src="<?php echo sanitize($val['pic']); ?>" alt="<?php echo sanitize($val['title']); ?>">
+                    </div>
+                    <div class="page-body">
+                        <p class="page-title"><?php echo sanitize($val['title']); ?> <span class="price">¥<?php echo sanitize(number_format($val['price'])); ?></span>
+                        </p>
+                    </div>
+                </a>
+            <?php
+            endforeach;
+            ?>
 
-    </div>
-    
-        <?php currentPagination($paginationNum,$dbProduct['total_page']);?>
+        </div>
+
+        <?php currentPagination($paginationNum, $dbProduct['total_page']); ?>
     </section>
 
 </body>
-    
