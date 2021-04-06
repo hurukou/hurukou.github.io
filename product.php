6ad90d13ae74516fc70d1490a53a8cc51f8d2194
Daisuke55 <daisukeippo@gmail.com>
2021-04-06 22:14:50 +0900

    testです

--- product.php
+++ product.php
@@ -1,7 +1,5 @@
 <?php
-
 require('function.php');
-
 //商品idのGETパラメータを取得
 $p_id = (!empty($_GET['p_id'])) ? $_GET['p_id'] : '';
 // DBから商品を取得
@@ -10,28 +8,28 @@ $getData = getProductInfo($p_id);
 //パラメータに不正な値が入っているかどうか
 //if(empty($getData)){
 //    error_log('エラー:指定ページに不正な値を検出');
-  //  header("Location:index.php");
+//  header("Location:index.php");
 //}
 
 
-if(!empty($_POST['submit'])){
-    
+if (!empty($_POST['submit'])) {
+
     require('auth.php');
-    
+
     try {
-        
+
         $db = db_connection();
-        
+
         $sql = 'INSERT INTO board (sale_user, buy_user, product_id, create_date) VALUES (:s_uid, :b_uid, :p_id, :date)';
         $data = array(':s_uid' => $getData['user_id'], ':b_uid' => $_SESSION['user_id'], ':p_id' => $p_id, ':date' => date('Y-m-d H:i:s'));
-        
+
         $stmt = queryex($db, $sql, $data);
-        
-        if($stmt){
+
+        if ($stmt) {
             $_SESSION['msg_suc'] = SUC05;
-            header("Location:msg.php?m_id=".$dbh->lastInsertID());
+            header("Location:msg.php?m_id=" . $dbh->lastInsertID());
         }
-    } catch(Exception $e){
+    } catch (Exception $e) {
         error_log('エラー:' . $e->getMessage());
         $err_msg['err_code'] = MSG07;
     }
@@ -43,80 +41,93 @@ require('head.php');
 
 <body class="page-productDetail page-1colum">
     <style>
-        #main .title{
+        #main .title {
             font-size: 28px;
             padding: 10px 0;
         }
-        .product-img-container{
+
+        .product-img-container {
             overflow: hidden;
         }
-        .product-img-container img{
+
+        .product-img-container img {
             width: 100%;
         }
-        .product-img-container .img-main{
+
+        .product-img-container .img-main {
             width: 750px;
             float: left;
             padding-right: 15px;
             box-sizing: border-box;
         }
-        .product-img-container .img-sub{
+
+        .product-img-container .img-sub {
             width: 230px;
             float: left;
             background: #f6f5f4;
             padding: 15px;
             box-sizing: border-box;
         }
-        .product-img-container .img-sub:hover{
+
+        .product-img-container .img-sub:hover {
             cursor: pointer;
         }
-        .product-img-container .img-sub img{
+
+        .product-img-container .img-sub img {
             margin-bottom: 15px;
         }
-        .product-img-container .img-sub img:last-child{
+
+        .product-img-container .img-sub img:last-child {
             margin-bottom: 0;
         }
-        .product-detail{
+
+        .product-detail {
             background: #f6f5f4;
             padding: 15px;
             margin-top: 15px;
             min-height: 150px;
         }
-        .product-buy{
+
+        .product-buy {
             overflow: hidden;
             margin-top: 15px;
             margin-bottom: 50px;
             height: 50px;
             line-height: 50px;
         }
-        .product-buy .product-left{
+
+        .product-buy .product-left {
             float: left;
         }
-        .product-buy .product-right{
+
+        .product-buy .product-right {
             float: right;
         }
-        .product-buy .price{
+
+        .product-buy .price {
             font-size: 32px;
             margin-right: 30px;
         }
-        .product-buy .btn{
+
+        .product-buy .btn {
             border: none;
             font-size: 18px;
             padding: 10px 30px;
         }
-        .product-buy .btn:hover{
+
+        .product-buy .btn:hover {
             cursor: pointer;
         }
-     
     </style>
-    
-   <?php
+
+    <?php
     require('header.php');
     ?>
-    
+
     <div id="contents" class="site-width">
-    
-        <section id="main" >
-    
+
+        <section id="main">
+
             <div class="product-img-container">
                 <div class="img-main">
                     <img src="<?php echo showImg(sanitize($getData['pic'])); ?>" alt="メイン画像：<?php echo sanitize($getData['title']); ?>">
@@ -124,8 +135,8 @@ require('head.php');
             </div>
             <div class="product-detail">
                 <p><?php echo sanitize($getData['content']); ?></p>
-                <p><?php echo sanitize($getData['ask']); ?></p> 
-                var_dump($getData);
+                <p><?php echo sanitize($getData['ask']); ?></p>
+                <?= var_dump($getData); ?>
 
             </div>
             <div class="product-buy">
@@ -143,3 +154,4 @@ require('head.php');
             </div>
 
         </section>
+</body>
--- prof.php
+++ prof.php
@@ -1,55 +1,55 @@
 <?php
 
 require('function.php');
-    
-    
+
+
 require('auth.php');
 
 $dbUserData = getUserdata($_SESSION['user_id']);
 
-if(!empty($_POST)){
-    
+if (!empty($_POST)) {
+
     $username = $_POST['username'];
     $pic = (!empty($_FILES['pic']['name'])) ?
-    uploadImg($_FILES['pic'],'pic') : '';
+        $uploadImg($_FILES['pic'], 'pic') : '';
     $pic = (empty($pic) && !empty($dbUserData['pic'])) ? $dbUserData['pic'] : $pic;
     $twitter = $_POST['twitter'];
     $youtube = $_POST['youtube'];
     $email = $_POST['email'];
     $results = $_POST['results'];
     $introduction = $_POST['introduction'];
-    
-    if($dbUserData['username'] !== $username){
+
+    if ($dbUserData['username'] !== $username) {
         validMax($username, 'username');
     }
-  if($dbUserData['twitter'] !== $twitter){
-      validMax($twitter, 'twitter');
-  }
-  if($dbUserData['youtube' !== $youtube]){
-      validMax($youtube, 'youtube');
-  }
-  if($dbUserData['email'] !== $email){
-      validMax($email, 'email');
-      
-      if(empty(err_msg['email'])){
-          validMaildup($email);
-      }
-  } 
-    
-    if(empty($err_msg)){
-        
-        try{
+    if ($dbUserData['twitter'] !== $twitter) {
+        validMax($twitter, 'twitter');
+    }
+    if ($dbUserData['youtube' !== $youtube]) {
+        validMax($youtube, 'youtube');
+    }
+    if ($dbUserData['email'] !== $email) {
+        validMax($email, 'email');
+
+        if (empty($err_msg['email'])) {
+            validMaildup($email);
+        }
+    }
+
+    if (empty($err_msg)) {
+
+        try {
             $pdo = db_connection();
             $sql = 'UPDATE users SET username = :u_name, twitter = :twitter, youtube = :youtube, email = :email, results = :results, introduction = :introduction,  pic = :pic WHERE id = :u_id';
             $data = array(':u_name' => $username, ':twitter' => $twitter, ':youtube' => $youtube, ':email' => $email, ':results' => $results, ':introduction' => $introduction, ':pic' => $pic, ':u_id' => $dbUserData['id']);
-            
+
             $stmt = queryex($pdo, $sql, $data);
-            
-            if($stmt){
+
+            if ($stmt) {
                 $_SESSION['msg_update'] = UPD02;
                 header("Location:mypage.php");
             }
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             error_log('エラー:' . $e->getMessage());
             $err_msg['err_code'] = MSG07;
         }
@@ -77,47 +77,47 @@ require('head.php');
 
                     <div class="err_code">
                         <?php
-                    if(!empty($err_msg['err_code'])) echo $err_msg['err_code'];
-                    ?>
+                        if (!empty($err_msg['err_code'])) echo $err_msg['err_code'];
+                        ?>
                     </div>
                     <span>名前<input type="text" name="username" value="<?php echo $getData('username'); ?>"></span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['username'])) echo $err_msg['username'];
-                    ?>
+                        if (!empty($err_msg['username'])) echo $err_msg['username'];
+                        ?>
                     </label>
                     <span class="profImg">プロフィール画像<input type="hidden" name="MAX_FILE_SIZE" value="3145728"><input type="file" name="pic" class="input-pic">
-                        <img src="<?php echo getData('pic'); ?>" alt="" class="input-img" style="<?php if(empty(getData('pic'))) echo 'display:none;' ?>">
+                        <img src="<?php echo $getData('pic'); ?>" alt="" class="input-img" style="<?php if (empty(getData('pic'))) echo 'display:none;' ?>">
                         ドラッグ＆ドロップ
                     </span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['pic'])) echo $err_msg['pic'];
-                    ?>
+                        if (!empty($err_msg['pic'])) echo $err_msg['pic'];
+                        ?>
                     </label>
                     <span>Twitter<input type="text" name="twitter" value="<?php echo $getData('twitter'); ?>"></span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['twitter'])) echo $err_msg['twitter'];
-                    ?>
+                        if (!empty($err_msg['twitter'])) echo $err_msg['twitter'];
+                        ?>
                     </label>
                     <span>Youtubeチャンネル<input type="text" name="youtube" value="<?php echo $getData('youtube'); ?>"></span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['youtube'])) echo $err_msg['youtube'];
-                    ?>
+                        if (!empty($err_msg['youtube'])) echo $err_msg['youtube'];
+                        ?>
                     </label>
                     <span>実績<textarea name="results" cols="50" rows="5"><?php echo $getData('results'); ?></textarea></span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['results'])) echo $err_msg['results'];
-                    ?>
+                        if (!empty($err_msg['results'])) echo $err_msg['results'];
+                        ?>
                     </label>
                     <span>自己紹介<textarea name="introduction" cols="50" rows="5"><?php echo $getData('introduction'); ?>"></textarea></span>
                     <label class="err_msg">
                         <?php
-                    if(!empty($err_msg['introduction'])) echo $err_msg['introduction'];
-                    ?>
+                        if (!empty($err_msg['introduction'])) echo $err_msg['introduction'];
+                        ?>
                     </label>
                     <div class="btn-container">
                         <input type="submit" class="btn prof" value="変更する">
